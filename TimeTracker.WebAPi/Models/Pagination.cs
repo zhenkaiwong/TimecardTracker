0d56de8e44f37594f61ed0b13de0ecd1a30146bc
zhenkaiwong1996 <zhenkaiwong.work@gmail.com>
2024-12-23 17:29:19 +0800

    refactor: rename property name to improve clarity

--- TimeTracker.WebAPi/Models/Pagination.cs
+++ TimeTracker.WebAPi/Models/Pagination.cs
@@ -1,9 +1,13 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TimeTracker.WebAPi.Models;
 
 public class Pagination
 {
-  public int Start { get; set; }
+  public int Skip { get; set; }
+  [Range(1, 100)]
   public int Rows { get; set; }
+  [MaxLength(250)]
+  public string Search { get; set; } = string.Empty;
 }
